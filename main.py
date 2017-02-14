6e022af8a88a45d225a600f05d8bd20cbf8e62f9
sergiy <sich-sergjj@rambler.ru>
2017-02-14 16:17:55 +0200

    code refactored

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ def parse_json():
 
 
 def get_image(url):
-    response = requests.get(url)
-    img = Image.open(BytesIO(response.content))
+    try:
+        response = requests.get(url)
+    finally:
+        img = Image.open(BytesIO(response.content))
     return img
 
 
@@ -33,42 +35,58 @@ def draw_text(canvas, title, subtitle, begin_time, end_time):
     draw.text((230, 50), subtitle, font=font_text2, fill="black")
 
 
-def create_tag(time_begin, time_end, image_link, title, subtitle):
+def optimize_image(image, k):
+    width = image.size[0] * k
+    height = image.size[1] * k
+    size = (width, height)
+    image.thumbnail(size)
+
+
+def draw_images(canvas, image, tag_height):
+    optimize_image(image, 0.15)
+    canvas.paste(image, (100, 0))
+
+    background = Image.new("RGB", (270, tag_height - 1), "#f33")
+    canvas.paste(background, (215, 0))
+
+
+def create_tag(time_begin, time_end, image_link, title, subtitle, tag_height):
     begin_time = get_time_string(time_begin)
     end_time = get_time_string(time_end)
+
     canvas = Image.new("RGB", (500, 200), "#fff")
 
     image = get_image(image_link)
-    size = (0.15 * image.size[0], 0.15 * image.size[1])
-    image.thumbnail(size)
-    canvas.paste(image, (100, 0))
 
-    background = Image.new("RGB", (270, 86), "#f33")
-    canvas.paste(background, (215, 0))
-
+    draw_images(canvas, image, tag_height)
     draw_text(canvas, title, subtitle, begin_time, end_time)
 
     return canvas
 
 
-def compose_tags(width, height, tags):
+def compose_tags(width, height, tags, tag_height):
     canvas = Image.new("RGB", (width, height), "#fff")
     offest_y = 0
     for tag in tags:
         canvas.paste(tag, (0, offest_y))
-        offest_y += 87
+        offest_y += tag_height
     canvas.save("tv_program.jpeg")
 
 
-def main():
-    programs = parse_json()
+def create_tags(programs):
+    tag_height = 87
     tags = []
     for program in programs:
         tag = create_tag(program["realtime_begin"], program["realtime_end"],
-                         program["image"]["preview"], program["title"], program["subtitle"])
+                         program["image"]["preview"], program["title"], program["subtitle"], tag_height)
         tags.append(tag)
-    compose_tags(485, len(programs)*87, tags)
+    return tags
 
 
-main()
+if __name__ == "__main__":
+    tag_height = 87
+    programs = parse_json()
+    tags = create_tags(programs)
+    compose_tags(485, len(programs) * tag_height, tags, tag_height)
+
 
