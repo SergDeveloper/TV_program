9f9cc994f69051c160fe9f168abb849ac67867c9
sergiy <sich-sergjj@rambler.ru>
2017-02-14 15:32:03 +0200

    task complete

--- main.py
+++ main.py
@@ -22,12 +22,33 @@ def get_time_string(time):
     return datetime.datetime.fromtimestamp(float(seconds)).strftime('%Y-%m-%d %H:%M:%S')[-8:-3]
 
 
-def draw_text():
-    pass
+def draw_text(canvas, title, subtitle, begin_time, end_time):
+    draw = ImageDraw.Draw(canvas)
+    font_text1 = ImageFont.truetype('arial.ttf', 16, encoding="unic")
+    font_text2 = ImageFont.truetype('arial.ttf', 14, encoding="unic")
+    font_time = ImageFont.truetype('arial.ttf', 14, encoding="unic")
+    draw.text((25, 30), begin_time, font=font_time, fill="black")
+    draw.text((25, 50), end_time, font=font_time, fill="black")
+    draw.text((220, 15), title, font=font_text1, fill="white")
+    draw.text((230, 50), subtitle, font=font_text2, fill="black")
 
 
-def create_tag():
-    pass
+def create_tag(time_begin, time_end, image_link, title, subtitle):
+    begin_time = get_time_string(time_begin)
+    end_time = get_time_string(time_end)
+    canvas = Image.new("RGB", (500, 200), "#fff")
+
+    image = get_image(image_link)
+    size = (0.15 * image.size[0], 0.15 * image.size[1])
+    image.thumbnail(size)
+    canvas.paste(image, (100, 0))
+
+    background = Image.new("RGB", (270, 86), "#f33")
+    canvas.paste(background, (215, 0))
+
+    draw_text(canvas, title, subtitle, begin_time, end_time)
+
+    return canvas
 
 
 def compose_tags(width, height, tags):
@@ -46,7 +67,7 @@ def main():
         tag = create_tag(program["realtime_begin"], program["realtime_end"],
                          program["image"]["preview"], program["title"], program["subtitle"])
         tags.append(tag)
-    compose_tags(485, 1900, tags)
+    compose_tags(485, len(programs)*87, tags)
 
 
 main()
